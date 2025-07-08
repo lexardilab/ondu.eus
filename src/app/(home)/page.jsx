6e0667de0c67d54b4d60402074ac970f99a3dda6
lexardi <lexardi.lab@gmail.com>
2025-07-08 16:54:09 +0200

    index

---
+++ src/app/(home)/page.jsx
@@ -0,0 +1,6 @@
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen"><h1>ondu</h1></div>
+  );
+}
--- src/app/page.jsx
+++
@@ -1,6 +0,0 @@
-
-export default function Home() {
-  return (
-    <div className="flex items-center justify-center h-full"><h1>ondu</h1></div>
-  );
-}
