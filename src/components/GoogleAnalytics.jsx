fa8e9406aa0e72c86eca90720254b724d960ca40
lexardi <lexardi.lab@gmail.com>
2025-07-10 12:14:32 +0200

    analytics

--- src/components/GoogleAnalytics.jsx
+++ src/components/GoogleAnalytics.jsx
@@ -5,17 +5,24 @@ import { useEffect } from "react";
 export default function GoogleAnalytics() {
   useEffect(() => {
     const consent = localStorage.getItem("cookiesAccepted");
-    if (consent === "true") {
-      // Añade el script de Google Analytics
+    if (consent === "true" && typeof window !== "undefined") {
+      // Carga el script de GA
       const script = document.createElement("script");
       script.src = "https://www.googletagmanager.com/gtag/js?id=G-QTZW800E7Q";
       script.async = true;
       document.head.appendChild(script);
 
-      window.dataLayer = window.dataLayer || [];
-      function gtag() { window.dataLayer.push(arguments); }
-      gtag('js', new Date());
-      gtag('config', 'G-QTZW800E7Q', { page_path: window.location.pathname });
+      // Configura GA
+      const inlineScript = document.createElement("script");
+      inlineScript.innerHTML = `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-QTZW800E7Q', {
+          page_path: window.location.pathname,
+        });
+      `;
+      document.head.appendChild(inlineScript);
     }
   }, []);
 
