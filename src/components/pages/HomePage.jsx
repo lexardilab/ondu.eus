796d1f9d88284c57f1647f64ee4c19896b370a50
lexardi <lexardi.lab@gmail.com>
2025-07-10 10:40:59 +0200

    imagen

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function HomePage({ data }) {
   return (
     <main className="flex items-center justify-center h-screen">
-        <Image src="/ondu_Home.jpg" width="400" height="600" alt="Ondu Zentroa"></Image>
+        <Image src="/ondu_home.jpg" width="400" height="600" alt="Ondu Zentroa"></Image>
     </main>
   );
 }
