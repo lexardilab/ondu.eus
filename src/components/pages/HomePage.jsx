6c0d176350a5d4f540800b3819bd3f6b045e0fee
lexardi <lexardi.lab@gmail.com>
2025-07-16 14:15:54 +0200

    index logo

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -4,10 +4,27 @@ import CookieBanner from "@/components/CookieBanner";
 export default function HomePage({ data, lang }) {
   return (
     <>
-      <main className="flex items-center justify-center h-screen">
-        <Image src="/home.svg" width={400} height={600} alt="Ondu Zentroa" />
+      <main className="flex items-center justify-center w-full h-screen bg-white">
+        <div
+          className="
+            w-[340px] h-[620px] 
+            sm:w-[320px] sm:h-[480px] 
+            md:w-[400px] md:h-[600px] 
+            relative 
+            mt-[-35vh] sm:mt-[-15vh] md:mt-0
+          "
+        >
+          <Image
+            src="/home.svg"
+            alt="Ondu Zentroa"
+            fill
+            className="object-contain"
+            priority
+          />
+        </div>
       </main>
       <CookieBanner lang={lang} />
     </>
   );
 }
+
