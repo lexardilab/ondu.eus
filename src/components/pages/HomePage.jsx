c1eeac5bb685e8ef1bb0ef16dcec17007cbbadf7
Lexardi Lab <lexardi.lab@gmail.com>
2025-09-30 12:56:30 +0200

    home

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,12 +1,12 @@
-import Image from 'next/image';
-import CookieBanner from '../CookieBanner';
+import Image from "next/image";
+import CookieBanner from "../CookieBanner";
 
 export default function HomePage({ data, lang }) {
   return (
     <>
       <main className="flex items-center justify-center px-12 pt-48">
         <Image
-          src="/ondu_home.jpg"
+          src="/home.webp"
           width={400}
           height={600}
           alt="Ondu Zentroa"
